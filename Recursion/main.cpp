15623b9d3db9c10a531d061f34cbd8c509091a18
Игорь Петров <igor567-1986@mail.ru>
2022-08-14 12:04:03 +0400

    class work

---
+++ Recursion/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void elevator(int floor);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int floor;
+	cout << "Введите номер этажа:"; cin >> floor;
+	elevator(floor);
+
+}
+void elevator(int floor)
+{
+	if (floor == 0)
+	{
+		cout << "Вы в подвале" << endl;
+		return;
+	}
+	cout << "Вы на " << floor << " этаже\n";
+	elevator(floor - 1);
+	cout << "Вы на " << floor << " этаже\n";
+}
